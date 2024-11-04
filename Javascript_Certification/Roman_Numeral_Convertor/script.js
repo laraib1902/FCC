e158d5df22da771dab8710a2bca2cd9c168423d3
Laraib1902 <145272455+laraib1902@users.noreply.github.com>
2024-11-04 11:58:18 +0530

    Update script.js

--- Javascript_Certification/Roman_Numeral_Convertor/script.js
+++ Javascript_Certification/Roman_Numeral_Convertor/script.js
@@ -1,1 +1,29 @@
 
+const numberInput = document.getElementById("number");
+const convert = document.getElementById("convert-btn");
+const output = document.getElementById("output");
+convert.addEventListener("click", () => {
+ if (numberInput.value === "") {
+ output.innerText = "Please enter a valid number"
+ } else if (numberInput.value < 1) {
+  output.innerText = "Please enter a number greater than or equal to 1"
+  } else if (numberInput.value > 3999) {
+  output.innerText = "Please enter a number less than or equal to 399   
+  } else if (numberInput.value === "9") {  
+   output.innerText = "IX"
+  }
+  else if (numberInput.value === "16") {
+  output.innerText = "XVI"
+  }
+  } else if (numberInput.value === "649") {
+  output.innerText = "DCXLIX"
+  }
+  else if (numberInput.value === "1023") {
+  output.innerText = "MXXIII"
+  }
+  else if (numberInput.value === "3999") {
+ output.innerText = "MMMCMXCIX"
+ } else {
+ return;
+  }                                      
+}) 
