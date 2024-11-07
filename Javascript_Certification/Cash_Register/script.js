bc214aca24fa5550ae204d480f429edd4d40802a
Laraib1902 <145272455+laraib1902@users.noreply.github.com>
2024-11-08 00:07:08 +0530

    Update script.js

--- Javascript_Certification/Cash_Register/script.js
+++ Javascript_Certification/Cash_Register/script.js
@@ -1,46 +1,63 @@
 let price = 3.26;
 let cid = [
+  
   ['PENNY', 1.01],
-    ['NICKEL', 2.05],
-      ['DIME', 3.1],
-        ['QUARTER', 4.25],
-          ['ONE', 90],
-            ['FIVE', 55],
-              ['TEN', 20],
-                ['TWENTY', 60],
-                  ['ONE HUNDRED', 100]
-                  ];
+   
+  ['NICKEL', 2.05],
+    
+  ['DIME', 3.1],
+        
+  ['QUARTER', 4.25],
+        
+  ['ONE', 90],
+          
+  ['FIVE', 55],
+           
+  ['TEN', 20],
+             
+  ['TWENTY', 60],
+              
+  ['ONE HUNDRED', 100]
+                
+];
 
-                  const displayChangeDue = document.getElementById('change-due');
-                  const cash = document.getElementById('cash');
-                  const purchaseBtn = document.getElementById('purchase-btn');
-                  const priceScreen = document.getElementById('price-screen');
-                  const cashDrawerDisplay = document.getElementById('cash-drawer-display');
+                 
+const displayChangeDue = document.getElementById('change-due');
+                
+const cash = document.getElementById('cash');
+                 
+const purchaseBtn = document.getElementById('purchase-btn');
+                 
+const priceScreen = document.getElementById('price-screen');
+                
+const cashDrawerDisplay = document.getElementById('cash-drawer-display');
 
-                  const formatResults = (status, change) => {
-                    displayChangeDue.innerHTML = `<p>Status: ${status}</p>`;
-                      displayChangeDue.innerHTML += change
-                          .map(
-                                ([denominationName, amount]) => `<p>${denominationName}: $${amount}</p>`
+                  
+const formatResults = (status, change) => {
+                   
+  displayChangeDue.innerHTML = `<p>Status: ${status}</p>`;
+                     
+  displayChangeDue.innerHTML += change
+                          
+    .map(([denominationName, amount]) => `<p>${denominationName}: $${amount}</p>`
                                     )
-                                        .join('');
-                                        };
+                                        
+    .join('');
+                                        
+};
 
-                                        const checkCashRegister = () => {
-                                          const cashInCents = Math.round(Number(cash.value) * 100);
-                                            const priceInCents = Math.round(price * 100);
-                                              if (cashInCents < priceInCents) {
-                                                  alert('Customer does not have enough money to purchase the item');
-                                                      cash.value = '';
-                                                          return;
-                                                            }
-
-                                                              if (cashInCents === priceInCents) {
-                                                                  displayChangeDue.innerHTML =
-                                                                        '<p>No change due - customer paid with exact cash</p>';
-                                                                            cash.value = '';
-                                                                                return;
-                                                                                  }
+                                       
+const checkCashRegister = () => {const cashInCents = Math.round(Number(cash.value) * 100);
+const priceInCents = Math.round(price * 100);
+ if (cashInCents < priceInCents) {alert('Customer does not have enough money to purchase the item');
+   cash.value = '';                            
+   return }
+  if (cashInCents === priceInCents) { displayChangeDue.innerHTML='<p>No change due - customer paid with exact cash</p>';
+    cash.value = '';
+                                                                                
+     return;
+                                                                                  
+}
 
                                                                                     let changeDue = cashInCents - priceInCents;
                                                                                       const reversedCid = [...cid]
